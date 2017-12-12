975283b0a8a924759bf5ba1b7d078540765d92fc
Simon <simon.h.rosner@gmail.com>
2017-12-12 14:41:46 -0500

    Added about us

---
+++ AboutUs.php
@@ -0,0 +1,17 @@
+<p>
+    The Artisan Trail project is a way to connect local artists with art fans.
+</p>
+<br/>
+<p>
+    The end goal of the project is to provide members of the Huntingdon
+    community with a website and Android app which connect to a database of
+    local art projects, performances, and events. The website and app will
+    simultaneously allow for artists, event coordinators, and local business
+    owners to make information on their endeavors publicaly available and easily
+    accessible.
+</p>
+<br/>
+<p>
+    The project is currently being developed by Juniata College
+    for the Huntingdon board of Tourism.
+</p>
