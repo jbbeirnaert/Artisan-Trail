cf424a7a29f4f1fefc1fae881c588911f66c13ed
Simon <simon.h.rosner@gmail.com>
2017-12-11 22:58:10 -0500

    Added single Event
    Edited Events to allow linking to individual events
    Added individual event pages with additional detail

---
+++ Events.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+<body>
+    <?php include('../dummyData.php'); ?>
+    
+<table border="1" cellpadding="10">
+    <thead><tr><th>Name</th><th>Host</th><th>Location</th><th>Start</th><th>End</th><th>Description</th><th>Price</th></tr></thead>
+    <tbody>
+    <?php
+    //Docs suggest that API will return single events or a list of event IDs
+    $eventIds = json_decode(getEventList(), true);
+    foreach($eventIds['eventlist'] as $key): 
+        $event = json_decode(getEvent($key));
+        $link = "Event.php?event_id=".$event->eventid;
+    ?>
+        <tr>
+            <td><?php 
+            echo "<a href='$link'>";
+            echo $event->name; 
+            echo "</a>";
+            ?></td>
+            <td><?php echo $event->hostname; ?></td>
+            <td><?php echo $event->location->address->name; ?></td>
+            <td><?php echo $event->startdatetime; ?></td>
+            <td><?php echo $event->enddatetime; ?></td>
+            <td><?php echo $event->description; ?></td>
+            <td><?php echo '$'. $event->price[0]->value; 
+            //Unsure how implementation of price reflects usage.
+            //I wrote this by strictly following the docs
+            //which is why price is an array
+            ?></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+
+</body>
+</html>
